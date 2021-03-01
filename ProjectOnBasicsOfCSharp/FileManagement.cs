f52c44b9740ecbb59d2ff683992971728c2a0bf4
vashah <vashah@teksystems.com>
2021-03-01 13:12:20 +0530

    Add project files.

---
+++ ProjectOnBasicsOfCSharp/FileManagement.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ProjectOnBasicsOfCSharp
+{
+    class FileManagement
+    {
+        public static void Do()
+        {
+            WriteTeacherDetails();
+            ReadTeacherDetails();
+        }
+        private static void WriteTeacherDetails()
+        {
+            string dir = Directory.GetCurrentDirectory();
+            string path = dir + "\\teacherData.txt";
+            Console.WriteLine(path);
+            if(File.Exists(path))
+            {
+                Console.WriteLine("File exists");
+            }
+            else
+            {
+                Console.WriteLine("file Does Not Exist");
+            }
+            Console.WriteLine("Adding content in the file...");
+            StreamWriter wrt = File.CreateText(path);
+            wrt.WriteLine("Ram, 30, CSE");
+            wrt.WriteLine("Sita, 25, ME");
+            wrt.WriteLine("Shyam, 35, Civil");
+            wrt.WriteLine("Geeta, 31, PE");
+            wrt.Close();
+            Console.WriteLine("Content has been added to the file.");
+        }
+
+        private static void ReadTeacherDetails()
+        {
+            string dir = Directory.GetCurrentDirectory();
+            string path = dir + "\\teacherData.txt";
+            Console.WriteLine("Reading File...");
+            if (File.Exists(path))
+            {
+                Console.WriteLine("File Exist");
+                var contents = File.ReadAllLines(path);
+                foreach(var content in contents)
+                {
+                    string[] data = content.Split(", ");
+                    string details = "Name: " + data[0] + ", Age: " + data[1] + ", Dept: " + data[2];
+                    Console.WriteLine(details);
+                }
+            }
+            else
+            {
+                Console.WriteLine("File Does not exist");
+            }
+        }
+    }
+}
---
+++ ProjectOnBasicsOfCSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProjectOnBasicsOfCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            FileManagement.Do();
+        }
+    }
+}
